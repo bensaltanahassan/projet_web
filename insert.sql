729379aae2241467b6d56cd5a98786461f94a32b
Bensaltana hassan <80928113+bensaltanahassan@users.noreply.github.com>
2023-06-22 10:43:43 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
 ('robe_1.jpg', 'Robe estivale à fleurs', 'Une magnifique robe estivale à fleurs avec un design léger et confortable. Parfait pour les journées ensoleillées.', 39.99, 10, 1),
-('robe_2.jpg', 'Robe longue bohème', 'Une élégante robe longue bohème avec des motifs colorés et un style décontracté. Idéale pour les soirées d'été.', 49.99, 8, 1),
+('robe_2.jpg', 'Robe longue bohème', 'Une élégante robe longue bohème avec des motifs colorés et un style décontracté. Idéale pour les soirées d été.', 49.99, 8, 1),
 ('robe_3.jpg', 'Robe sans manches en lin', 'Une robe sans manches en lin avec une coupe fluide et légère. Parfaite pour rester fraîche et élégante en été.', 34.99, 15, 1),
 ('robe_4.jpg', 'Robe midi à pois', 'Une robe midi élégante avec un motif à pois et une ceinture ajustable. Parfaite pour les occasions spéciales.', 54.99, 5, 1),
 ('robe_5.jpg', 'Robe d'été à volants', 'Une robe d'été légère avec des volants et une coupe décontractée. Parfaite pour les journées chaudes à la plage.', 29.99, 12, 1);
@@ -21,7 +21,7 @@ INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id
 
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
 ('chemisierhomme_1.jpg', 'Chemise à carreaux', 'Une chemise à carreaux classique et intemporelle pour un look décontracté. Parfaite pour toutes les saisons.', 39.99, 10, 4),
-('chemisierhomme_2.jpg', 'Chemise en lin', 'Une chemise en lin légère et respirante, parfaite pour les journées chaudes d'été. Un choix élégant et confortable.', 49.99, 8, 4),
+('chemisierhomme_2.jpg', 'Chemise en lin', 'Une chemise en lin légère et respirante, parfaite pour les journées chaudes d été. Un choix élégant et confortable.', 49.99, 8, 4),
 ('chemisierhomme_3.jpg', 'Chemise à manches courtes', 'Une chemise à manches courtes pour un look décontracté et estival. Idéale pour les occasions informelles.', 34.99, 12, 4),
 ('chemisierhomme_4.jpg', 'Chemise à col mao', 'Une chemise à col mao pour un look moderne et élégant. Parfaite pour ajouter une touche de sophistication à votre tenue.', 42.99, 9, 4),
 ('chemisierhomme_5.jpg', 'Chemise en denim', 'Une chemise en denim résistante et polyvalente. Un choix intemporel pour un look décontracté et branché.', 44.99, 15, 4);
@@ -31,17 +31,17 @@ INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id
 ('chemisierfemme_2.jpg', 'Chemisier en soie', 'Un chemisier en soie luxueux et confortable. Ajoutez une touche de glamour à votre tenue avec cette pièce élégante.', 49.99, 8, 5),
 ('chemisierfemme_3.jpg', 'Chemisier à volants', 'Un chemisier à volants pour un style romantique et féminin. Parfait pour les journées ensoleillées ou les occasions spéciales.', 34.99, 12, 5),
 ('chemisierfemme_4.jpg', 'Chemisier à rayures', 'Un chemisier à rayures classique et intemporel. Parfait pour un look élégant et raffiné au quotidien.', 42.99, 9, 5),
-('chemisierfemme_5.jpg', 'Chemisier à manches courtes', 'Un chemisier à manches courtes pour un look décontracté et estival. Polyvalent et confortable, il s'adapte à toutes les occasions.', 39.99, 15, 5);
+('chemisierfemme_5.jpg', 'Chemisier à manches courtes', 'Un chemisier à manches courtes pour un look décontracté et estival. Polyvalent et confortable, il s adapte à toutes les occasions.', 39.99, 15, 5);
 
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
 ('shorts_1.jpg', 'Short en jean', 'Un short en jean décontracté et polyvalent. Parfait pour les journées ensoleillées et les looks décontractés.', 24.99, 15, 6),
-('shorts_2.jpg', 'Short en lin', 'Un short en lin léger et respirant. Idéal pour les journées chaudes d'été et les activités de plein air.', 29.99, 12, 6),
+('shorts_2.jpg', 'Short en lin', 'Un short en lin léger et respirant. Idéal pour les journées chaudes d été et les activités de plein air.', 29.99, 12, 6),
 ('shorts_3.jpg', 'Short à motifs', 'Un short à motifs colorés et amusants. Ajoutez une touche de fantaisie à votre tenue estivale avec ce short original.', 27.99, 10, 6),
 ('shorts_4.jpg', 'Short en coton', 'Un short en coton confortable et polyvalent. Parfait pour un look décontracté et décontracté au quotidien.', 22.99, 20, 6),
 ('shorts_5.jpg', 'Short en denim', 'Un short en denim classique et intemporel. Un incontournable de la garde-robe estivale pour un look tendance.', 26.99, 18, 6);
 
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
-('jupe_1.jpg', 'Jupe plissée', 'Une jupe plissée élégante et féminine. Parfaite pour les occasions spéciales ou pour ajouter une touche d'élégance à votre tenue quotidienne.', 39.99, 15, 7),
+('jupe_1.jpg', 'Jupe plissée', 'Une jupe plissée élégante et féminine. Parfaite pour les occasions spéciales ou pour ajouter une touche d élégance à votre tenue quotidienne.', 39.99, 15, 7),
 ('jupe_2.jpg', 'Jupe en jean', 'Une jupe en jean décontractée et polyvalente. Idéale pour les looks décontractés et les journées ensoleillées.', 34.99, 12, 7),
 ('jupe_3.jpg', 'Jupe midi à pois', 'Une jupe midi à pois pour un look rétro et chic. Ajoutez une touche de sophistication à votre tenue.', 42.99, 10, 7),
 ('jupe_4.jpg', 'Jupe en cuir', 'Une jupe en cuir pour un look audacieux et moderne. Parfaite pour les soirées et les occasions spéciales.', 59.99, 8, 7),
@@ -52,7 +52,7 @@ INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id
 ('accessoire_2.jpg', 'Chapeau de paille', 'Un chapeau de paille à larges bords pour vous protéger du soleil avec style.', 19.99, 15, 8),
 ('accessoire_3.jpg', 'Lunettes de soleil aviateur', 'Des lunettes de soleil aviateur avec monture en métal pour un look rétro et cool.', 24.99, 18, 8),
 ('accessoire_4.jpg', 'Foulard imprimé', 'Un foulard coloré et léger pour accessoiriser vos tenues estivales.', 14.99, 25, 8),
-('accessoire_5.jpg', 'Ceinture en cuir', 'Une ceinture en cuir avec boucle métallique pour ajouter une touche d'élégance à votre tenue.', 22.99, 20, 8);
+('accessoire_5.jpg', 'Ceinture en cuir', 'Une ceinture en cuir avec boucle métallique pour ajouter une touche d élégance à votre tenue.', 22.99, 20, 8);
 
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
 ('sandale_1.jpg', 'Sandales à lanières', 'Des sandales à lanières élégantes et confortables pour un look estival.', 49.99, 15, 9),
@@ -63,7 +63,7 @@ INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id
 
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
 ('tongs_1.jpg', 'Tongs classiques', 'Des tongs classiques et confortables pour les journées estivales.', 14.99, 20, 10),
-('tongs_2.jpg', 'Tongs en caoutchouc', 'Des tongs en caoutchouc résistantes à l'eau pour la plage et la piscine.', 12.99, 25, 10),
+('tongs_2.jpg', 'Tongs en caoutchouc', 'Des tongs en caoutchouc résistantes à l eau pour la plage et la piscine.', 12.99, 25, 10),
 ('tongs_3.jpg', 'Tongs à imprimé tropical', 'Des tongs avec un imprimé tropical pour un look exotique et coloré.', 16.99, 18, 10),
 ('tongs_4.jpg', 'Tongs à semelle épaisse', 'Des tongs avec une semelle épaisse pour un meilleur soutien et confort.', 19.99, 15, 10),
 ('tongs_5.jpg', 'Tongs en cuir', 'Des tongs en cuir pour un look plus sophistiqué et élégant.', 24.99, 12, 10);
@@ -83,8 +83,8 @@ INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id
 ('pantalon_5.jpg', 'Pantalon à rayures', 'Un pantalon léger à rayures, pour un look estival et sophistiqué.', 54.99, 20, 12);
   
 INSERT INTO `database`.`product` (`image`, `name`, `desc`, `price`, `stock`, `id_category`) VALUES
-('tenue_sport_1.jpg', 'Ensemble de sport leggings et brassière', 'Un ensemble de sport composé d'un legging et d'une brassière, parfait pour vos séances d'entraînement.', 79.99, 15, 13),
-('tenue_sport_2.jpg', 'T-shirt de sport respirant', 'Un t-shirt de sport respirant, idéal pour rester au frais pendant l'effort.', 29.99, 20, 13),
+('tenue_sport_1.jpg', 'Ensemble de sport leggings et brassière', 'Un ensemble de sport composé d un legging et d une brassière, parfait pour vos séances d,entraînement.', 79.99, 15, 13),
+('tenue_sport_2.jpg', 'T-shirt de sport respirant', 'Un t-shirt de sport respirant, idéal pour rester au frais pendant l effort.', 29.99, 20, 13),
 ('tenue_sport_3.jpg', 'Short de sport léger', 'Un short de sport léger et confortable, pour une totale liberté de mouvement.', 39.99, 18, 13),
 ('tenue_sport_4.jpg', 'Débardeur de sport', 'Un débardeur de sport ajusté et stylé, pour une allure dynamique pendant vos séances.', 24.99, 15, 13),
 ('tenue_sport_5.jpg', 'Pantalon de jogging', 'Un pantalon de jogging doux et confortable, parfait pour vos activités sportives en extérieur.', 49.99, 12, 13);
